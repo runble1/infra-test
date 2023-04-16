ee75c3db020aca7bba27e27e139d371fde88ee09
runble1 <nishiyama-wataru@dmm.com>
2023-04-17 07:54:45 +0900

    add

--- modules/alb/alb.tf
+++ modules/alb/alb.tf
@@ -92,7 +92,7 @@ resource "aws_security_group" "alb" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/32"]
   }
 
   tags = {
@@ -106,5 +106,5 @@ resource "aws_security_group_rule" "alb_http" {
   protocol          = "tcp"
   security_group_id = aws_security_group.alb.id
   type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/32"]
 }
